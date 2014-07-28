8d16a2dd7f20d7be2f9ead16434bdd053d41cbaa
Srinivas Prasad Gumdelli <srinivasgumdelli@gmail.com>
2014-07-27 21:18:40 -0500

    Adds Exception handling for invalid request type

--- Client/ClientAPI.py
+++ Client/ClientAPI.py
@@ -24,13 +24,15 @@ class ClientAPI:
     def __repr__(self):
         return 'ClientAPi(base_url=%s, user_id=%s, api_key=%s)' % (self.base_url, self.user_id, self.api_key)
 
-    @staticmethod
-    def request_method(payload):
+    def request_method(self, payload):
         """
 
-        :rtype : object
-        :param base_url: The base url for the project
+        :rtype: object
         :param payload: The payload that needs to be passed in as arguments
         :return: Returns response objects for now
         """
-        return requests.get(ClientAPI.base_url,payload)
+        effective_url = self.base_url + payload
+        response = requests.get(effective_url)
+        if response.status_code == 200:
+            return response.content
+        response.raise_for_status()
